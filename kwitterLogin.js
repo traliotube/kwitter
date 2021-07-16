52540355f119e266bf733b28b5ddb5a8d8b778fb
Bunny Pranav <pranavvasipalli@gmail.com>
2021-07-16 16:48:52 +0530

    Made login work

--- kwitterLogin.js
+++ kwitterLogin.js
@@ -1,3 +1,17 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA5PRytVpniBn7qyAmxKrozhTps3ZqC7PQ",
+  authDomain: "kwitter-c6df5.firebaseapp.com",
+  databaseURL: "https://kwitter-c6df5-default-rtdb.firebaseio.com",
+  projectId: "kwitter-c6df5",
+  storageBucket: "kwitter-c6df5.appspot.com",
+  messagingSenderId: "275849529985",
+  appId: "1:275849529985:web:e7f762ab57e8a1a841db80",
+  measurementId: "G-2EQ2KKCG62",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 function addUser() {
   var userName = document.getElementById("username").value;
   firebase
