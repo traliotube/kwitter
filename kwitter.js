552cedbbb6c238228e05d8d13c8304645cffb002
Bunny Pranav <pranavvasipalli@gmail.com>
2021-07-18 14:57:11 +0530

    Made Firebase Sync, Added room functions

--- kwitter.js
+++ kwitter.js
@@ -1,16 +1,28 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyA5PRytVpniBn7qyAmxKrozhTps3ZqC7PQ",
-  authDomain: "kwitter-c6df5.firebaseapp.com",
-  databaseURL: "https://kwitter-c6df5-default-rtdb.firebaseio.com",
-  projectId: "kwitter-c6df5",
-  storageBucket: "kwitter-c6df5.appspot.com",
-  messagingSenderId: "275849529985",
-  appId: "1:275849529985:web:e7f762ab57e8a1a841db80",
-  measurementId: "G-2EQ2KKCG62",
+  apiKey: "AIzaSyAqMy9zxCpqeNYIi_nlTV_j0eA8cYi33bM",
+  authDomain: "kwitter-6e059.firebaseapp.com",
+  projectId: "kwitter-6e059",
+  databaseURL: "https://kwitter-6e059-default-rtdb.firebaseio.com",
+  storageBucket: "kwitter-6e059.appspot.com",
+  messagingSenderId: "372577202138",
+  appId: "1:372577202138:web:879f52a6bbff251067dae3",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+
+username = localStorage.getItem("userName");
+document.getElementById("usernameOutput").innerHTML = `Welcome ${username}!`;
+
+function addRoomName() {
+  var localRoomName = document.getElementById("roomname").value;
+  firebase
+    .database()
+    .ref("/")
+    .child(localRoomName)
+    .set({ purpose: "adding room name" });
+  localStorage.setItem("roomName", localRoomName);
+  window.location = "kwitter.html";
+}
 
 function getData() {
   firebase
@@ -22,9 +34,17 @@ function getData() {
         childKey = childSnapshot.key;
         Room_names = childKey;
         //Start code
-
+        console.log("Room Name - " + Room_names);
+        row = `<div class='room_name' id='${Room_names}' onclick='redirectRoom(this.id)'>#${Room_names}</div><hr>`;
+        document.getElementById("output").innerHTML += row;
         //End code
       });
     });
 }
 getData();
+
+function redirectRoom(name) {
+  console.log(name);
+  localStorage.setItem("room_name", name);
+  window.location = "kwitter_page.html";
+}
